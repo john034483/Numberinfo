324912ae0bb6dd15a1fbced732abc87a82aca7a9
john034483 <sunana2009ids@outlook.com>
2026-06-09 16:24:35 +0530

    Add files via upload

---
+++ api/Sun_.js
@@ -0,0 +1,17 @@
+{
+    "success": true,
+    "developer": "https://t.me/botadminshere",
+    "credit": "https://t.me/Toxicadminn",
+    "private": "https://t.me/+14rDlunTEzwwZGY1",
+    "result": {
+        "pan_info": {
+            "dob": "1977-03-23",
+            "gender": "M",
+            "monthly_income": "25000",
+            "name": "JAIRAM SHAW",
+            "pan_number": "BOKPS7056D",
+            "phone": "****Hidden****"
+        },
+        "success": true
+    }
+}
